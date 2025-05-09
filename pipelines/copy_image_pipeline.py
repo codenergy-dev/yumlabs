4d2b3eab8bce99f16ae8033e38654b6742f04e46
Lucas Lannes <lucaslannesr@gmail.com>
2025-05-09 16:49:23 -0300

    Update copy_image_pipeline

--- pipelines/copy_image_pipeline.py
+++ pipelines/copy_image_pipeline.py
@@ -1,9 +1,14 @@
-import shutil
+import os
+from PIL import Image
 
 def copy_image_pipeline(
   image: str,
-  target: str,
+  name: str = None,
+  output_dir: str = "output",
   **kwargs,
 ):
-  shutil.copy2(image, target)
-  return { "image": target }
+  image_file = Image.open(image)
+  output = os.path.join(output_dir, (image if name is None else name).split("/")[-1])
+  os.makedirs(output_dir, exist_ok=True)
+  image_file.save(output)
+  return { "image": output }
