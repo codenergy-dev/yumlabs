76da19e50bbe4b307b7f07bd4c6efc8747b57d3d
Lucas Lannes <lucaslannesr@gmail.com>
2025-05-17 20:00:05 -0300

    Add tile ControlNet to load_pipeline

--- pipelines/load_pipeline.py
+++ pipelines/load_pipeline.py
@@ -43,6 +43,8 @@ def load_pipeline(
       controlnet_model.append(ControlNetModel.from_pretrained("lllyasviel/control_v11e_sd15_shuffle", torch_dtype=torch_dtype, use_safetensors=True))
     elif 'seg' in preprocessor and model in ["stable-diffusion-v1-5/stable-diffusion-v1-5", "Lykon/AnyLoRA", "admruul/anything-v3.0", "Lykon/dreamshaper-7", "Lykon/dreamshaper-8", "proximasanfinetuning/fantassified_icons_v2"]:
       controlnet_model.append(ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-seg", torch_dtype=torch_dtype, use_safetensors=True))
+    elif 'tile' in preprocessor and model in ["stable-diffusion-v1-5/stable-diffusion-v1-5", "Lykon/AnyLoRA", "admruul/anything-v3.0", "Lykon/dreamshaper-7", "Lykon/dreamshaper-8", "proximasanfinetuning/fantassified_icons_v2", "Anomaly-Games-Inc/aziibpixelmix-json"]:
+      controlnet_model.append(ControlNetModel.from_pretrained("lllyasviel/control_v11f1e_sd15_tile", torch_dtype=torch_dtype))
     elif 'zoe' in preprocessor and model in ["stable-diffusion-v1-5/stable-diffusion-v1-5", "Lykon/AnyLoRA", "admruul/anything-v3.0", "Lykon/dreamshaper-7", "Lykon/dreamshaper-8", "proximasanfinetuning/fantassified_icons_v2"]:
       controlnet_model.append(ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-depth", torch_dtype=torch_dtype, use_safetensors=True))
     elif 'canny' in preprocessor and model == "stabilityai/stable-diffusion-xl-base-1.0":
