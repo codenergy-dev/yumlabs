00dc00058850924770c3feb845c8ccdef133e643
Lucas Lannes <lucaslannesr@gmail.com>
2025-05-17 23:01:18 -0300

    Convert to RGB in invert_color_pipeline

--- pipelines/invert_color_pipeline.py
+++ pipelines/invert_color_pipeline.py
@@ -6,7 +6,7 @@ def invert_color_pipeline(
   output_dir: str = "output",
   **kwargs,
 ):
-  image_file = Image.open(image)
+  image_file = Image.open(image).convert("RGB")
   output = os.path.join(output_dir, "invert_color_pipeline.png")
   pipe = ImageOps.invert(image_file)
   pipe.save(output)
