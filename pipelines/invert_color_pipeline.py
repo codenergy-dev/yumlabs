b4feb7a264c48aacb24f355a789d0e515f1f9227
Lucas Lannes <lucaslannesr@gmail.com>
2025-05-17 22:23:24 -0300

    Add invert_color_pipeline

---
+++ pipelines/invert_color_pipeline.py
@@ -0,0 +1,13 @@
+import os
+from PIL import Image, ImageOps
+
+def invert_color_pipeline(
+  image: str,
+  output_dir: str = "output",
+  **kwargs,
+):
+  image_file = Image.open(image)
+  output = os.path.join(output_dir, "invert_color_pipeline.png")
+  pipe = ImageOps.invert(image_file)
+  pipe.save(output)
+  return { "image": output }
