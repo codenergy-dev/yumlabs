fad8f9ad694bd871e46d522fccad49f5837a80b5
Lucas Lannes <lucaslannesr@gmail.com>
2025-12-03 17:24:55 -0300

    Convert PNG to JPG automatically in copy_image_pipeline

--- pipelines/copy_image_pipeline.py
+++ pipelines/copy_image_pipeline.py
@@ -2,13 +2,23 @@ import os
 from PIL import Image
 
 def copy_image_pipeline(
-  image: str,
-  name: str = None,
-  output_dir: str = "output",
-  **kwargs,
+    image: str,
+    name: str = None,
+    output_dir: str = "output",
+    **kwargs,
 ):
-  image_file = Image.open(image)
-  output = os.path.join(output_dir, (image if name is None else name).split("/")[-1])
-  os.makedirs(output_dir, exist_ok=True)
-  image_file.save(output)
-  return { "image": output }
+    image_file = Image.open(image)
+
+    output_name = (image if name is None else name).split("/")[-1]
+    output = os.path.join(output_dir, output_name)
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    ext = output_name.lower().split(".")[-1]
+
+    if ext in ("jpg", "jpeg") and image_file.mode == "RGBA":
+        image_file = image_file.convert("RGB")
+
+    image_file.save(output)
+
+    return {"image": output}
